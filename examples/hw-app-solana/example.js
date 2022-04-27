75cad5973f7148caf242bb21b119469f860930b0
Andrew Hlynskyi <ahlincq@gmail.com>
2022-04-28 02:51:11 +0300

    Added hw-app-solana sign test

--- examples/hw-app-solana/example.js
+++ examples/hw-app-solana/example.js
@@ -1,5 +1,6 @@
 const Transport = require("@ledgerhq/hw-transport-node-hid").default;
 const Solana = require("@ledgerhq/hw-app-solana").default;
+const solana = require("@solana/web3.js");
 const { program } = require("commander");
 const bs58 = require("bs58");
 
@@ -7,9 +8,9 @@ const bs58 = require("bs58");
     program
         .command("config")
         .action(async () => {
-            var transport = await Transport.create();
-            var ledger = await new Solana(transport);
-            let config = await ledger.getAppConfiguration();
+            const transport = await Transport.create();
+            const ledger = new Solana(transport);
+            const config = await ledger.getAppConfiguration();
             console.log("--- config:", config);
         });
 
@@ -18,13 +19,57 @@ const bs58 = require("bs58");
         .action(async (path) => {
             if (!path) path = "0'";
             //console.log(path);
-            var transport = await Transport.create();
-            var ledger = await new Solana(transport);
-            let r = await ledger.getAddress(`44'/501'/${path}`);
+            const transport = await Transport.create();
+            const ledger = new Solana(transport);
+            const r = await ledger.getAddress(`44'/501'/${path}`);
             console.log("--- address:", r.address);
             const pubkey_string = bs58.encode(r.address);
             console.log("--- pubkey:", pubkey_string);
         });
 
+    program
+        .command("sign <from_wallet_idx> <to_wallet_idx>")
+        .action(async (from_idx, to_idx) => {
+            const transport = await Transport.create();
+            const ledger = new Solana(transport);
+            const from_path = `44'/501'/${from_idx}'`;
+            const form_address = (await ledger.getAddress(from_path)).address;
+            console.log("--- from pubkey:", bs58.encode(form_address));
+            const to_path = `44'/501'/${to_idx}'`;
+            const to_address = (await ledger.getAddress(to_path)).address;
+            console.log("---   to pubkey:", bs58.encode(to_address));
+
+            const from_pubkey = new solana.PublicKey(form_address);
+            const to_pubkey = new solana.PublicKey(to_address);
+            const ix = solana.SystemProgram.transfer({
+                fromPubkey: from_pubkey,
+                toPubkey: to_pubkey,
+                lamports: 42,
+            });
+
+            // XXX: Fake blockhash so this example doesn't need a
+            // network connection. It should be queried from the
+            // cluster in normal use.
+            const recentBlockhash = bs58.encode(Buffer.from(Array(32).fill(3)));
+
+            let tx = new solana.Transaction({
+                recentBlockhash,
+                feePayer: from_pubkey,
+            })
+                .add(ix);
+
+            const msg_data = tx.serializeMessage();
+            const sig_bytes = (await ledger.signTransaction(from_path, msg_data)).signature;
+
+            const sig_string = bs58.encode(sig_bytes);
+            console.log("--- len:", sig_bytes.length, "sig:", sig_string);
+
+            tx.addSignature(from_pubkey, sig_bytes);
+            console.log("--- verifies:", tx.verifySignatures());
+
+            const connection = new solana.Connection(solana.clusterApiUrl('mainnet-beta'));
+            console.log("--- fee:", await tx.getEstimatedFee(connection));
+        });
+
     await program.parseAsync();
-})()
+})().catch(e => console.log(e));
